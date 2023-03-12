da469c6e449f7f7541e00883e2fee588b8f0d5ea
Manjeet Singh <manjeetazim@gmail.com>
2023-03-12 17:54:42 +0530

    JS prac

---
+++ ps1.js
@@ -0,0 +1,23 @@
+let s = "Manjeet";
+let b = 4;
+s = s + b;
+console.log(s);
+console.log(typeof s);
+
+const a = "Zorage";
+// a = 4;
+console.log(a);
+
+const c = {
+    "name": "Zorage",
+    "Age": 19,
+}
+c['name']="Manjeet"
+console.log(c)
+
+const dict={
+    'age': "how many years a person has lived since his birth",
+    'appreciate':"apporve worthiness of something  or someone"
+}
+
+console.log(dict.age,"--" ,dict.appreciate)
---
+++ ps2.js
@@ -0,0 +1,9 @@
+//q1
+let age = prompt("enter age")
+if(age>10 && age<20){
+    console.log('yes')
+}
+else {
+    console.log('no')
+}
+
